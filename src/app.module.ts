00397e55604bcfdefd4455ed885ce529817c4da4
Gustavo DeLlac <gustavodellac@gmail.com>
2025-09-03 01:16:32 -0300

    update dockerignore, update gitignore, update docker-compose.dev servicio mysql y volumen anonimo pnpm-store, update dockerfile.dev para instalar cli de nestjs y setear ruta store pnpm, se crea plantilla para api restful cats nest, se establece conexion a mysql por medio de paquete typeorm, se quitan archivos innecesarios

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CatsModule } from './cats/cats.module';
+
+const host = process.env.MYSQL_HOST;
+const port = process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : undefined;
+const user = process.env.MYSQL_USER;
+const password = process.env.MYSQL_PASSWORD;
+const database =  process.env.MYSQL_DATABASE;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CatsModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: host,
+      port: port,
+      username: user,
+      password: password,
+      database: database,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
