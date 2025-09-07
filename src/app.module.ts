0b70a54660c805b92ab8d0ef32336c5fb0231aae
Gustavo DeLlac <gustavodellac@gmail.com>
2025-09-07 18:01:05 +0000

    se agrega configuracion de devcontainer para ejecutar vscode dentro del contenedor. Se agrega git al contenedor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 
 const host = process.env.MYSQL_HOST;
-const port = process.env.MYSQL_PORT ? parseInt(process.env.MYSQL_PORT) : undefined;
+const port = process.env.MYSQL_PORT
+  ? parseInt(process.env.MYSQL_PORT)
+  : undefined;
 const user = process.env.MYSQL_USER;
 const password = process.env.MYSQL_PASSWORD;
-const database =  process.env.MYSQL_DATABASE;
+const database = process.env.MYSQL_DATABASE;
 
 @Module({
   imports: [
     CatsModule,
     TypeOrmModule.forRoot({
-      type: "mysql",
+      type: 'mysql',
       host: host,
       port: port,
       username: user,
