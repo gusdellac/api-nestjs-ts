55b9247a1c79651e8080e8a97e5007cbcb0dbece
Gustavo DeLlac <gustavodellac@gmail.com>
2025-09-16 03:04:18 +0000

    se agrega JWT_SECRET var entorno. Se agrega conf icon nest devcontainer. Se agrega guard middleware para validacion de autorizacion endpoint profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
-import { log } from 'console';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './guard/auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +29,11 @@ export class AuthController {
     loginDto: LoginDto,
   ) {
     return this.authService.login(loginDto);
+  }
+
+  @Get('profile')
+  @UseGuards(AuthGuard)
+  profile(@Request() req) {
+    return req.user;
   }
 }
--- src/auth/constants/jwt.constants.ts
+++ src/auth/constants/jwt.constants.ts
@@ -1,3 +1,3 @@
 export const jwtConstants = {
-  secret: 'yourSecretKey', // In production, use environment variables
+  secret: process.env.JWT_SECRET, // In production, use environment variables
 };
