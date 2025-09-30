b944a6da805ae88ec2e70cdcadf9001e14b72f2e
Gustavo DeLlac <gustavodellac@gmail.com>
2025-09-29 22:53:23 -0300

    se expone docs api en endpoint json

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -24,6 +25,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  // Servir JSON en /docs-json
+  app.use('/docs-json', (req: Request, res: Response) => {
+    res.json(document);
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
